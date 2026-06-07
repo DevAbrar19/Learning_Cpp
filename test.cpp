b8f2438568d6bee08f9df89398adebe8de50f6f0
Ajwad_Abrar <ajwadabrar15@gmail.com>
2026-06-07 16:40:57 +0600

    Switch cases

--- test.cpp
+++ test.cpp
@@ -2,6 +2,35 @@
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+    int month;
+
+    std::cout << "Enter your month: ";
+    std::cin >> month;
+
+    switch (month)
+    {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        std::cout << "Summer";
+        break;
+
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+        std::cout << "Autumn";
+        break;
+
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+        std::cout << "Winter";
+        break;
+
+    default:
+        std::cout << "Bogus Month";
+    }
 }
