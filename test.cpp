acf05706e695979279dc9d8312e5c8cbda4468e5
Ajwad_Abrar <ajwadabrar15@gmail.com>
2026-06-07 17:44:12 +0600

    For loops

--- test.cpp
+++ test.cpp
@@ -2,29 +2,21 @@
 
 int main()
 {
-    double temp;
-    char unit;
+    int row, col;
 
-    std::cout << "Enter c for Celsius and f for Fahrenheit : ";
-    std::cin >> unit;
+    std::cout << "Enter rows: ";
+    std::cin >> row;
 
-    std::cout << "Enter temp : ";
-    std::cin >> temp;
+    std::cout << "Enter columns: ";
+    std::cin >> col;
 
-    if (unit == 'c')
+    for (int i = 1; i <= row; i++)
     {
-        double f = ((9 * temp) / (double)5) + 32;
-
-        std::cout << "Converted temp if Fahrenheit is " << f << std::endl;
-    }
-    else if (unit == 'f')
-    {
-        double c = (temp - 32) * (5 / (double)9);
-        std::cout << "Converted temp if Celsius is " << c << std::endl;
-    }
-    else
-    {
-        std::cout << "Wrong input";
+        for (int j = 1; j <= col; j++)
+        {
+            std::cout << "* ";
+        }
+        std::cout << "\n";
     }
 
     return 0;
